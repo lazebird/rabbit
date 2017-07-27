b7c6c7bcd35788bc985d4fec2835959226b55b52
lazebird <lazebird@163.com>
2017-07-27 08:28:25 +0800

    Add files via upload

---
+++ ping/ping/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.ListBox();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "间隔(ms)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "次数";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1000";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(299, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "-1";
+            // 
+            // output
+            // 
+            this.output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.output.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.FormattingEnabled = true;
+            this.output.ItemHeight = 19;
+            this.output.Location = new System.Drawing.Point(31, 186);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(412, 118);
+            this.output.TabIndex = 5;
+            // 
+            // toolStripContainer1
+            // 
+            this.toolStripContainer1.BottomToolStripPanelVisible = false;
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.AutoScroll = true;
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.textBox4);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.label3);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.button2);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.textBox3);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.button1);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.output);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.textBox2);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.textBox1);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.label2);
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.label1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(474, 320);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.LeftToolStripPanelVisible = false;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.RightToolStripPanelVisible = false;
+            this.toolStripContainer1.Size = new System.Drawing.Size(474, 320);
+            this.toolStripContainer1.TabIndex = 8;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            this.toolStripContainer1.TopToolStripPanelVisible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(98, 23);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(345, 22);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "www.mozilla.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "地址";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(359, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 32);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "日志文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(31, 109);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(322, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(359, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 37);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 320);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ping";
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox output;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ping/ping/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Taskbar;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.IO;
+
+namespace ping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            records = new int[5];
+            Thread taskbar_th = new Thread(taskbar_dll_detect_safe);
+            taskbar_th.IsBackground = true;
+            taskbar_th.Start();
+        }
+        StreamWriter file;
+        string addr;
+        int state, timeout, times;
+        Thread ping_thread;
+        int txcnt, rxcnt, losscnt;
+        int mintm, maxtm, totaltm;
+        int[] records;
+        int recordidx;
+        int taskbar_support;
+
+        private void taskbar_dll_detect_safe()
+        {
+            try
+            {
+                taskbar_dll_detect();
+                taskbar_support = 1;
+            }
+            catch (FileNotFoundException e)
+            {
+                logprint("TaskbarManager DLL missed: " + e.FileName);
+                taskbar_support = 0;
+            }
+        }
+        private void taskbar_dll_detect()
+        {
+            TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.NoProgress);
+        }
+        private void display_taskbar_safe(int rx)
+        {
+            if (taskbar_support == 1)
+            {
+                display_taskbar(rx);
+            }
+        }
+        private void display_taskbar(int rx)
+        {
+            if (!TaskbarManager.IsPlatformSupported)
+            {
+                return;
+            }
+            if (recordidx < 5)
+            {
+                records[recordidx++] = rx;
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    records[i] = records[i + 1];
+                }
+                records[4] = rx;
+            }
+            int count = 0;
+            for (int i = 0; i < recordidx; i++)
+            {
+                count += records[i];
+            }
+            if (count != recordidx)
+            {
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Error);
+            }
+            else
+            {
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Normal);
+            }
+            TaskbarManager.Instance.SetProgressValue(count, 5);
+        }
+        private void display_statistics()
+        {
+            state = 0;
+            button1.Text = "开始";
+            button1.Refresh();
+            float lossper, avgtm;
+            if (txcnt != 0)
+            {
+                lossper = (float)losscnt / txcnt * 100;
+            }
+            else
+            {
+                lossper = 0;
+            }
+            if (rxcnt != 0)
+            {
+                avgtm = (float)totaltm / rxcnt;
+            }
+            else
+            {
+                avgtm = 0;
+            }
+            logprint("发送 = " + txcnt + "，接收 = " + rxcnt + "，丢失 = " + losscnt + " (" + lossper.ToString() + " % 丢失)");
+            logprint("最短 = " + mintm + "毫秒，最长 = " + maxtm + "毫秒，平均 = " + avgtm.ToString() + "毫秒");
+        }
+        private void ping_process()
+        {
+            timeout = int.Parse(textBox1.Text);
+            times = int.Parse(textBox2.Text);
+            addr = textBox4.Text;
+            Ping pingSender = new Ping();
+            PingReply reply;
+            output.Items.Clear();
+            txcnt = rxcnt = losscnt = 0;
+            mintm = maxtm = totaltm = 0;
+            if (addr == "")
+            {
+                state = 0;
+                button1.Text = "停止";
+                button1.Refresh();
+                logprint("地址错误。");
+                return;
+            }
+            while (times < 0 || times-- > 0)
+            {
+                try
+                {
+
+                    reply = pingSender.Send(addr, timeout);
+                    txcnt++;
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        rxcnt++;
+                        logprint("来自 " + reply.Address + " 的回复: 字节=" + reply.Buffer.Length + " 毫秒=" + reply.RoundtripTime + " TTL=" + reply.Options.Ttl);
+                        display_taskbar_safe(1);
+                        if (timeout > (int)reply.RoundtripTime)
+                        {
+                            Thread.Sleep(timeout - (int)reply.RoundtripTime);
+                        }
+
+                        if (mintm == 0)
+                        {
+                            mintm = (int)reply.RoundtripTime;
+                        }
+                        else
+                        {
+                            mintm = Math.Min(mintm, (int)reply.RoundtripTime);
+                        }
+                        if (maxtm == 0)
+                        {
+                            maxtm = (int)reply.RoundtripTime;
+                        }
+                        else
+                        {
+                            maxtm = Math.Max(maxtm, (int)reply.RoundtripTime);
+                        }
+                        totaltm += (int)reply.RoundtripTime;
+                    }
+                    else
+                    {
+                        losscnt++;
+                        logprint("请求超时。");
+                        display_taskbar_safe(0);
+                    }
+                }
+                catch (PingException e)
+                {
+                    logprint("错误: " + e.Message);
+                    return;
+                }
+            }
+            display_statistics();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (state == 0)
+            {
+                recordidx = 0;
+                state = 1;
+                button1.Text = "停止";
+                button1.Refresh();
+                // start
+                CheckForIllegalCrossThreadCalls = false;
+                ping_thread = new Thread(ping_process);
+                ping_thread.IsBackground = true;
+                ping_thread.Start();
+            }
+            else
+            {
+                state = 0;
+                button1.Text = "开始";
+                button1.Refresh();
+                // stop
+                times = 0;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog filename = new SaveFileDialog();
+            //filename.InitialDirectory = Application.StartupPath;
+            filename.Filter = "文本文件 (*.txt)|*.txt|All files (*.*)|*.*";
+            filename.RestoreDirectory = true;
+            filename.CreatePrompt = true;
+            filename.OverwritePrompt = true;
+            if (filename.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = filename.FileName;
+            }
+        }
+        private void logprint(string msg)
+        {
+            if (textBox3.Text != "")
+            {
+                file = new StreamWriter(textBox3.Text, true);
+                file.WriteLine(msg);
+                file.Close();
+            }
+            output.Items.Add(msg);
+            output.TopIndex = output.Items.Count - (int)(output.Height / output.ItemHeight);
+            output.Refresh();
+        }
+    }
+}
