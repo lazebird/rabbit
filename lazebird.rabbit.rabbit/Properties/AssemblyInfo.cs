438f26a2a04400244ed85c9f14f1770995436416
lazebird <lazebird@gmail.com>
2018-06-28 15:38:27 +0800

    more

--- lazebird.rabbit.rabbit/Properties/AssemblyInfo.cs
+++ lazebird.rabbit.rabbit/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Rabbit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("lazebird")]
 [assembly: AssemblyProduct("rabbit")]
 [assembly: AssemblyCopyright("Copyright ©  2017-2018")]
 [assembly: AssemblyTrademark("")]
