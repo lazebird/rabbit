f8c3a3d46370e666dbb5191a3a7b5bfd8e9996bb
lazebird <lazebird@gmail.com>
2018-07-29 23:05:32 +0800

    change rfs

--- lazebird.rabbit.http/rhttpd.cs
+++ lazebird.rabbit.http/rhttpd.cs
@@ -1,11 +1,8 @@
 ﻿using lazebird.rabbit.fs;
-using lazebird.rabbit.queue;
 using System;
 using System.Collections;
 using System.IO;
 using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace lazebird.rabbit.http
@@ -13,15 +10,12 @@ namespace lazebird.rabbit.http
     public class rhttpd : IDisposable
     {
         Action<string> log;
-        HttpListener httpListener = null;
+        HttpListener hl = null;
         Hashtable mimehash;
-        Hashtable fhash;
         rfs rfs;
         public rhttpd(Action<string> log)
         {
             this.log = log;
-            fhash = new Hashtable();
-            fhash.Add("/", new rfile("dir", "", 0, DateTime.Today));
             rfs = new rfs(log);
         }
         public void init_mime(string value)
@@ -32,177 +26,84 @@ namespace lazebird.rabbit.http
             {
                 string[] e = mime.Split(':');
                 if (e.Length == 2)
-                {
                     mimehash.Add(e[0], e[1]);
-                }
             }
 
         }
-        public bool start(int port)
+        public void start(int port)
         {
-            httpListener = new HttpListener();
-            httpListener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
-            httpListener.Prefixes.Clear();
-            httpListener.Prefixes.Add(string.Format("http://+:{0}/", port));
+            hl = new HttpListener();
+            hl.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+            hl.Prefixes.Clear();
+            hl.Prefixes.Add(string.Format("http://+:{0}/", port));
             try
             {
-                httpListener.Start();
-                httpListener.BeginGetContext(new AsyncCallback(Dispather), null);
-                return true;
+                hl.Start();
+                hl.BeginGetContext(new AsyncCallback(dispatcher), null);
             }
             catch (Exception e)
             {
                 log("!E: " + e.Message);
-                httpListener.Close();
-                return false;
+                hl.Close();
             }
         }
         public void stop()
         {
             Dispose();
         }
-        string get_suffix(string fname)
+        void session_task(HttpListenerRequest request, HttpListenerResponse response)
         {
-            int idx = fname.LastIndexOf(".");
-            if (idx > 0)
-            {
-                return fname.Substring(idx);
-            }
-            return "";
+            ss s = new ss(log, request, response, rfs);
+            s.proc_req(mimehash);
         }
-        string get_mime(string suffix)
-        {
-            return (string)mimehash[suffix] ?? (string)mimehash["*"] ?? "";
-        }
-        void Dispather(IAsyncResult ar)
+        void dispatcher(IAsyncResult ar)
         {
             try
             {
-                HttpListenerContext context = httpListener.EndGetContext(ar);
-                HttpListenerRequest request = context.Request;
-                HttpListenerResponse response = context.Response;
-                response.ContentEncoding = Encoding.UTF8;
-                string uri = Uri.UnescapeDataString(request.RawUrl);
-                log("I: " + request.HttpMethod + " " + uri);
-                byte[] buffer = null;
-
-                if (fhash.ContainsKey(uri) && ((rfile)fhash[uri]).type == "file")
-                {
-                    Thread t = new Thread(() => file_load(response, uri));
-                    t.IsBackground = true;
-                    t.Start();
-                }
-                else if (!fhash.ContainsKey(uri))
-                {
-                    buffer = Encoding.UTF8.GetBytes("404");
-                    response.ContentLength64 = buffer.Length;
-                    response.OutputStream.Write(buffer, 0, buffer.Length);
-                    response.OutputStream.Close();
-                }
-                else if (((rfile)fhash[uri]).type == "dir")
-                {
-                    buffer = Encoding.UTF8.GetBytes(dir_load(uri));
-                    response.ContentLength64 = buffer.Length;
-                    response.OutputStream.Write(buffer, 0, buffer.Length);
-                    response.OutputStream.Close();
-                }
-                httpListener.BeginGetContext(new AsyncCallback(Dispather), null);
+                HttpListenerContext context = hl.EndGetContext(ar);
+                Thread t = new Thread(() => session_task(context.Request, context.Response));
+                t.IsBackground = true;
+                t.Start();
+                hl.BeginGetContext(new AsyncCallback(dispatcher), null);
             }
             catch (Exception e)
             {
                 log("!E: " + e.Message);
             }
         }
-        void file_load(HttpListenerResponse response, string path)
-        {
-            Stream output = response.OutputStream;
-            response.ContentType = get_mime(get_suffix(path));
-            //log("Info: response suffix " + get_suffix(uri) + " ContentType " + response.ContentType);
-            FileStream fs = new FileStream((string)((rfile)fhash[path]).path, FileMode.Open, FileAccess.Read);
-            response.ContentLength64 = fs.Length;
-            rqueue q = new rqueue(10); // 10 * 10M, max memory used 100M
-            Thread t1 = new Thread(() => rfs.readstream(fs, q, 10000000));    // 10000000, max block size 10M
-            t1.IsBackground = true;
-            t1.Start();
-            rfs.writestream(output, q, fs.Name);
-            t1.Abort();
-            q.Dispose();
-        }
-        string dir_load(string path)
-        {
-            string index = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body><table><tbody>";
-            index += "<tr>" +
-                "<th>Name</th>" +
-                "<th>Size (Bytes)</th>" +
-                "<th>Last Modified</th>" +
-                "</tr>";
-            index += "<tr>" +
-              "<td><a href=" + Uri.EscapeUriString(path.Substring(0, path.Substring(0, path.Length - 1).LastIndexOf('/') + 1)) + ">" + ".." + " </a></td>" +
-              "<td>" + "</td>" +
-              "<td>" + "</td>" +
-              "</tr>";
-            Regex rgright = new Regex("^" + path + ".");
-            Regex rgwrong = new Regex("^" + path + ".+/.");
-            foreach (string key in fhash.Keys)
-            {
-                if (!rgright.IsMatch(key) || rgwrong.IsMatch(key))
-                {
-                    continue;
-                }
-                index += "<tr>" +
-                 "<td><a href=" + Uri.EscapeUriString(key) + ">" + key.Substring(path.Length) + " </a></td>" +
-                 "<td>" + (((rfile)fhash[key]).size).ToString("###,###") + "</td>" +
-                 "<td>" + ((rfile)fhash[key]).tm + "</td>" +
-                 "</tr>";
-            }
-            index += "</tbody></table></body></html>";
-            return index;
-        }
+
         string rootpath;
         public void set_root(string path)
         {
-            if (path == null || path == "" || this.rootpath == path)
-            {
-                return;
-            }
+            if (path == null || path == "" || this.rootpath == path) return;
             this.rootpath = path;
             log("R: " + path);
-            fhash = new Hashtable();
-            fhash.Add("/", new rfile("dir", "", 0, DateTime.Today));
-            DirectoryInfo dir = new DirectoryInfo(path);
-            foreach (FileInfo f in dir.GetFiles())
-            {
-                rfs.addfile(fhash, "/", f.FullName);
-            }
-            foreach (DirectoryInfo d in dir.GetDirectories())
-            {
-                rfs.adddir(fhash, "/", d.FullName, true);
-            }
+            //log("I: parent " + Path.GetDirectoryName(path) + " " + Path.GetFileName(path));
+            rfs.adddir("/", path);
         }
         public void add_dir(string path)
         {
-            rfs.adddir(fhash, "/", path, true);
+            rfs.adddir("/" + Path.GetDirectoryName(path), path);
         }
         public void del_dir(string path)
         {
-            rfs.deldir(fhash, "/", path);
+            rfs.deldir("/" + Path.GetDirectoryName(path));
         }
         public void add_file(string path)
         {
-            rfs.addfile(fhash, "/", path);
+            rfs.addfile("/", path);
         }
         public void del_file(string path)
         {
-            rfs.delfile(fhash, "/", path);
+            rfs.delfile("/" + Path.GetFileName(path));
         }
 
         protected virtual void Dispose(bool disposing)
         {
-            if (httpListener != null) httpListener.Close();
-            httpListener = null;
+            if (hl != null) hl.Close();
+            hl = null;
             if (!disposing) return;
             mimehash = null;
-            fhash = null;
             rfs = null;
         }
         public void Dispose()
---
+++ lazebird.rabbit.http/ss.cs
@@ -0,0 +1,127 @@
+﻿using lazebird.rabbit.fs;
+using lazebird.rabbit.queue;
+using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace lazebird.rabbit.http
+{
+    class ss
+    {
+        Action<string> log;
+        HttpListenerRequest request;
+        HttpListenerResponse response;
+        rfs rfs;
+        string method;
+        string uri;
+        rqueue q;
+        Thread t;
+
+        public ss(HttpListenerRequest request, HttpListenerResponse response, rfs rfs)
+        {
+            this.log = log_func;
+            this.request = request;
+            this.response = response;
+            this.rfs = rfs;
+            this.method = request.HttpMethod;
+            this.uri = Uri.UnescapeDataString(request.RawUrl);
+            response.ContentEncoding = Encoding.UTF8;
+        }
+        public ss(Action<string> log, HttpListenerRequest request, HttpListenerResponse response, rfs rfs) : this(request, response, rfs)
+        {
+            this.log = log;
+        }
+        void log_func(string msg) { }
+        string uri2filepath(string uri)
+        {
+            if (rfs.fhash.ContainsKey(uri)) return (string)rfs.fhash[uri];
+            string dname = uri;
+            string fname = "";
+            while (dname != null) // longest match
+            {
+                log("I: uri " + uri + " search " + dname + "//" + fname);
+                if (rfs.dhash.ContainsKey(dname)) return (string)rfs.dhash[dname];
+                fname = Path.GetFileName(dname) + "/" + fname;
+                dname = Path.GetDirectoryName(dname);
+            }
+            if (rfs.dhash.ContainsKey(uri)) return (string)rfs.dhash[uri];
+            if (rfs.dhash.ContainsKey(Path.GetDirectoryName(uri))) return (string)rfs.dhash[Path.GetDirectoryName(uri)] + Path.GetFileName(uri);
+            return null;
+        }
+        string uri2mime(Hashtable mimehash, string uri)
+        {
+            string fname = Path.GetFileName(uri);
+            string suffix = "";
+            int idx = fname.LastIndexOf(".");
+            if (idx >= 0)
+                suffix = fname.Substring(idx);
+            return (string)mimehash[suffix] ?? (string)mimehash["*"] ?? "";
+        }
+        void loadfile(Hashtable mimehash, string path)
+        {
+            Stream output = response.OutputStream;
+            response.ContentType = uri2mime(mimehash, uri);
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            response.ContentLength64 = fs.Length;
+            q = new rqueue(10); // 10 * 10M, max memory used 100M
+            t = new Thread(() => rfs.readstream(fs, q, 10000000));    // 10000000, max block size 10M
+            t.IsBackground = true;
+            t.Start();
+            rfs.writestream(output, q, fs.Name);
+            t.Abort();
+            q.Dispose();
+        }
+        void loaddir(string path)
+        {
+            string index = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body><table><tbody>";
+            index += "<tr>" +
+                "<th>Name</th>" +
+                "<th>Size (Bytes)</th>" +
+                "<th>Last Modified</th>" +
+                "</tr>";
+            index += "<tr>" +
+              "<td><a href=" + Uri.EscapeUriString(path.Substring(0, path.Substring(0, path.Length - 1).LastIndexOf('/') + 1)) + ">" + ".." + " </a></td>" +
+              "<td>" + "</td>" +
+              "<td>" + "</td>" +
+              "</tr>";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (FileInfo f in dir.GetFiles())
+                index += "<tr>" +
+                 "<td><a href=" + Uri.EscapeUriString(f.FullName) + ">" + f.Name + " </a></td>" +
+                 "<td>" + f.Length.ToString("###,###") + "</td>" +
+                 "<td>" + f.LastWriteTime + "</td>" +
+                 "</tr>";
+            foreach (DirectoryInfo d in dir.GetDirectories())
+                index += "<tr>" +
+                 "<td><a href=" + Uri.EscapeUriString(d.FullName) + ">" + d.Name + " </a></td>" +
+                 "<td>" + "-" + "</td>" +
+                 "<td>" + d.LastWriteTime + "</td>" +
+                 "</tr>";
+            index += "</tbody></table></body></html>";
+            byte[] buf = Encoding.UTF8.GetBytes(index);
+            response.ContentLength64 = buf.Length;
+            response.OutputStream.Write(buf, 0, buf.Length);
+            response.OutputStream.Close();
+        }
+        void loaderror(string path, int errno)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(errno + ": " + path);
+            response.ContentLength64 = buffer.Length;
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.OutputStream.Close();
+        }
+        public void proc_req(Hashtable mimehash)
+        {
+            string path = uri2filepath(uri);
+            if (File.Exists(path)) // file
+                loadfile(mimehash, path);
+            else if (Directory.Exists(path)) // dir
+                loaddir(path);
+            else
+                loaderror(path, 404);
+        }
+    }
+}
