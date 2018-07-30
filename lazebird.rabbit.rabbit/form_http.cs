7e143561023a4c4397cb3e7bdefa3cc9810b5419
lazebird <lazebird@gmail.com>
2018-07-31 00:03:03 +0800

    more

--- lazebird.rabbit.rabbit/form_http.cs
+++ lazebird.rabbit.rabbit/form_http.cs
@@ -1,6 +1,9 @@
 ﻿using lazebird.rabbit.common;
 using lazebird.rabbit.http;
 using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace lazebird.rabbit.rabbit
@@ -16,12 +19,13 @@ namespace lazebird.rabbit.rabbit
             httpd.init_mime(rconf.get("mime"));
             btn_httpd.Click += new EventHandler(httpd_click);
             btn_http_dir.Click += new EventHandler(httpd_dir_click);
+            init_pipeserver();
         }
         void httpd_log_func(string msg)
         {
             httpdlog.write(msg);
         }
-         void httpd_click(object sender, EventArgs evt)
+        void httpd_click(object sender, EventArgs evt)
         {
             if (((Button)btnhash["httpd_btn"]).Text == Language.trans("开始"))
             {
@@ -36,7 +40,7 @@ namespace lazebird.rabbit.rabbit
             }
             saveconf(); // save empty config to restore default config
         }
-         void httpd_dir_click(object sender, EventArgs e)
+        void httpd_dir_click(object sender, EventArgs e)
         {
             FolderBrowserDialog dialog = new FolderBrowserDialog();
             dialog.SelectedPath = Environment.CurrentDirectory;
@@ -45,6 +49,36 @@ namespace lazebird.rabbit.rabbit
             {
                 text_http_dir.Text = dialog.SelectedPath;
             }
+        }
+        void init_pipeserver()
+        {
+            Thread t = new Thread(pipesrv_task);
+            t.IsBackground = true;
+            t.Start();
+        }
+        void pipesrv_task()
+        {
+            try
+            {
+                NamedPipeServerStream pipeServer = new NamedPipeServerStream("lazebird.rabbit.rabbit", PipeDirection.In);
+                pipeServer.WaitForConnection();
+                pipeServer.ReadMode = PipeTransmissionMode.Byte;
+                StreamReader Reader = new StreamReader(pipeServer);
+                while (true)
+                {
+                    string msg = Reader.ReadLine();
+                    if (string.IsNullOrEmpty(msg)) break;
+                    httpd_log_func("I: Read pipe " + msg);
+                }
+            }
+            catch (Exception e)
+            {
+                httpd_log_func("!E: " + e.ToString());
+            }
+        }
+        void reg_shell()
+        {
+            httpshell hs = new httpshell();
         }
     }
 }
