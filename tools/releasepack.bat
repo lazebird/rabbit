0cc8df99331cde8e024136fab4ff8437afc3551c
lazebird <lazebird@gmail.com>
2018-07-31 21:21:06 +0800

    add release dir

---
+++ tools/releasepack.bat
@@ -0,0 +1,8 @@
+REM @echo off
+pushd %~dp0
+cd ../lazebird.rabbit.rabbit/bin/Release/
+echo %cd%
+set pwd=%cd%
+..\..\..\tools\ILMerge.exe /targetplatform:v4 /ndebug /target:winexe /out:../../../release/sRabbit.exe lazebird.rabbit.rabbit.exe /wildcards *.dll zh-CN/lazebird.rabbit.rabbit.resources.dll
+popd
+pause
