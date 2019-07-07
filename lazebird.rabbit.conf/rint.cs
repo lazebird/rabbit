0dbfe6d210d75f54833ff17464ee9befc376efcf
lazebird <lazebird@163.com>
2019-07-07 16:14:56 +0800

    more fix for upgrade problem

--- lazebird.rabbit.conf/rint.cs
+++ lazebird.rabbit.conf/rint.cs
@@ -5,6 +5,7 @@ namespace lazebird.rabbit.conf
     public class rint
     {
         string name;
+        int val = 0;
         Func<string, int> getcb;
         Action<string, int> setcb;
         public rint(string name, Func<string, int> get, Action<string, int> set)
@@ -13,7 +14,7 @@ namespace lazebird.rabbit.conf
             this.getcb = get;
             this.setcb = set;
         }
-        public int get(string name) { if (getcb != null) return getcb(name); return 0; }
-        public void set(string name, int val) { if (setcb != null) setcb(name, val); }
+        public int get(string name) { if (getcb != null) return getcb(name); return val; }
+        public void set(string name, int val) { if (setcb != null) setcb(name, val); this.val = val; }
     }
 }
--- lazebird.rabbit.conf/rstr.cs
+++ lazebird.rabbit.conf/rstr.cs
@@ -5,6 +5,7 @@ namespace lazebird.rabbit.conf
     public class rstr
     {
         string name;
+        string val = "unknown";
         Func<string, string> getcb;
         Action<string, string> setcb;
         public rstr(string name, Func<string, string> get, Action<string, string> set)
@@ -13,7 +14,7 @@ namespace lazebird.rabbit.conf
             this.getcb = get;
             this.setcb = set;
         }
-        public string get(string name) { if (getcb != null) return getcb(this.name); return "unknown"; }
-        public void set(string name, string val) { if (setcb != null) setcb(this.name, val); }
+        public string get(string name) { if (getcb != null) return getcb(this.name); return val; }
+        public void set(string name, string val) { if (setcb != null) setcb(this.name, val); this.val = val; }
     }
 }
